e9f97df5d2af28e5bc713275e8ad68692efe172f
caio-gabriel-cruz <killua37ptgmai.com>
2025-01-07 22:33:35 -0300

    change in splash

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const html = document.querySelector("html");
-const splash = document.querySelector(".splash");
+// const html = document.querySelector("html");
+// const splash = document.querySelector(".splash");
 
-html.classList.remove("desktop");
-html.classList.add("mobile");
-setTimeout(() => {
-  splash.style.opacity = 0;
-}, 700);
-splash.addEventListener("transitionend", () => {
-  splash.style.display = "none";
-});
+// html.classList.remove("desktop");
+// html.classList.add("mobile");
+// setTimeout(() => {
+//   splash.style.opacity = 0;
+// }, 700);
+// splash.addEventListener("transitionend", () => {
+//   splash.style.display = "none";
+// });

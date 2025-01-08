b7119e5cc9aa979aaa02e07f1627839925269780
caio-gabriel-cruz <killua37ptgmai.com>
2025-01-07 22:22:09 -0300

    change in splash transition

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 const html = document.querySelector("html");
 const splash = document.querySelector(".splash");
 
-if (window.innerWidth <= 768) {
-  html.classList.remove("desktop");
-  html.classList.add("mobile");
-  setTimeout(() => {
-    splash.style.opacity = 0;
-  }, 1000);
-  splash.addEventListener("transitionend", () => {
-    splash.style.display = "none";
-  });
-} else if (html.classList.contains("desktop")) {
+html.classList.remove("desktop");
+html.classList.add("mobile");
+setTimeout(() => {
+  splash.style.opacity = 0;
+}, 700);
+splash.addEventListener("transitionend", () => {
   splash.style.display = "none";
-}
+});
